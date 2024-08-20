e163df0065f9a656b48cb8427abeb0336284d618
redwane23 <redoianekassouse@gmail.com>
2024-08-20 02:56:26 +0100

    Delete mysite directory

--- mysite/home/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from home.models import Question
-
-class QuestionForm(forms.ModelForm):
-    class Meta:
-        model = Question
-        fields =['question_text','pub_date','image']
