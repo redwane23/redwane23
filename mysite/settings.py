71d5ca8c033353fdab607413a94e390aa1eb061a
redwane23 <redoianekassouse@gmail.com>
2024-08-22 14:43:54 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,6 +88,7 @@ DATABASES = {
         conn_max_age=600
     )
 }
+SECRET_KEY = os.getenv('SECRET_KEY')
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
