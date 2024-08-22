4f926a85592775dc49cbe0655815b4104e7f66eb
redwane23 <redoianekassouse@gmail.com>
2024-08-22 15:11:12 +0100

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,10 @@ from django.conf.urls.static import static
 
 app_name="home"
 urlpatterns = [
-    path('admin/', admin.site.urls,name='home'),
-    path("", include("home.urls")),
-    path('account/',include("account.urls"))
+    path('admin/', admin.site.urls),  
+    path('home/', include('home.urls')),  
+    path('account/', include('account.urls')), 
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+
