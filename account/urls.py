95af4f0f99bedee246522dae44c09eb1a9aef0ea
redwane23 <redoianekassouse@gmail.com>
2024-08-22 23:21:11 +0100

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from account import views
 from .views import CustomLoginView
 urlpatterns = [
-    path("create_account/",views.creat_account,name='register'),
+    path("create_account/",views.create_account,name='register'),
     path("login/",CustomLoginView.as_view(),name='login'),
 ]
