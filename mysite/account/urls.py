c3522814a0c36dbb64b0b0a08e1dadf00cc883f6
redwane23 <redoianekassouse@gmail.com>
2024-08-22 14:21:47 +0100

    Delete mysite directory

--- mysite/account/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from account import views
-from .views import CustomLoginView
-urlpatterns = [
-    path("create_account/",views.creat_account,name='register'),
-    path("login/",CustomLoginView.as_view(),name='login'),
-]
