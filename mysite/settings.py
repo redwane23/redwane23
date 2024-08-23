d8934a1a14c71e033f626e2f7d1428600ecd95df
redwane23 <redoianekassouse@gmail.com>
2024-08-23 22:38:32 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "home/static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL='/media/'
