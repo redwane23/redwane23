cff514f37851bbdad2a1a095bdb407aaf606a00b
redwane23 <redoianekassouse@gmail.com>
2024-08-23 13:29:47 +0100

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,65 +1,52 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, render,redirect
+from .models import Question,Choice
+from django.http import  HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
-from django.utils import timezone
-from django.contrib.auth.decorators import login_required
 from django.views import generic
-from django.utils.decorators import async_only_middleware
-from .models import Question, Choice
+from django.utils import timezone
 from .forms import QuestionForm
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 import IP2Location
+from django.http import JsonResponse
 
-# Initialize IP2Location database
 db = IP2Location.IP2Location("db/IP2LOCATION-LITE-DB1.BIN")
 
-# Synchronous function to get country
 def get_country(request):
     user_ip = request.META.get('REMOTE_ADDR')  # Get the user's IP address
     record = db.get_all(user_ip)
     country = record.country_short
+
     return country
 
 class IndexView(generic.ListView):
     template_name = "home/index.html"
     context_object_name = "question_list"
-    users = User.objects.filter(is_superuser=False)
+    users=User.objects.filter(is_superuser=False)
+    country=get_country
+    extra_context = {'users': users,'country':country}
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by
+        ("-pub_date")
 
-    async def get_queryset(self):
-        # Handle synchronous database operations correctly
-        queryset = await database_sync_to_async(Question.objects.filter)(
-            pub_date__lte=timezone.now()
-        ).order_by("-pub_date")
-        return queryset
-
-    async def get(self, request, *args, **kwargs):
-        # Call the synchronous function in an async view
-        country = get_country(request)
-        context = {
-            'users': self.users,
-            'country': country,
-        }
-        return await super().get(request, *args, **kwargs)
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "home/detail.html"
-
-    async def get_queryset(self):
-        queryset = await database_sync_to_async(Question.objects.filter)(
-            pub_date__lte=timezone.now()
-        )
-        return queryset
-
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "home/results.html"
 
-async def vote(request, question_id):
-    question = await database_sync_to_async(get_object_or_404)(Question, pk=question_id)
+    
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = await database_sync_to_async(question.choice_set.get)(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
@@ -72,22 +59,20 @@ async def vote(request, question_id):
         )
     else:
         selected_choice.votes = F("votes") + 1
-        await database_sync_to_async(selected_choice.save)()
+        selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("home:results", args=(question.id,)))
-
 @login_required
-@async_only_middleware
-async def add_question(request):
+def add_question(request):
     if request.method == 'POST':
-        form = QuestionForm(request.POST, request.FILES)
+        form = QuestionForm(request.POST,request.FILES)
         if form.is_valid():
-            form.instance.user = request.user
-            await database_sync_to_async(form.save)()
+            form.instance.user = request.user 
+            form.save()
             return redirect("/home")
     else:
-        form = QuestionForm()
-    return render(request, 'addquestion.html', {'form': form})
+        form=QuestionForm()
+    return render(request,'addquestion.html',{'form':form})
 
