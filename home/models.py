266b3a4fe9581713dd6b37f0d5d69cd01b52feca
redwane23 <redoianekassouse@gmail.com>
2024-08-24 00:24:54 +0100

    Update models.py

--- home/models.py
+++ home/models.py
@@ -3,8 +3,11 @@ import datetime
 from django.utils import timezone
 from django.contrib import admin
 from django.contrib.auth.models import User
-# Create your models here.
+from django.db import models
 
+class MyModel(models.Model):
+    image = models.ImageField(upload_to='images/')
+    
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
