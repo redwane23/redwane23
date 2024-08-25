65042d6b0fadfa5ba24443ae599d0bda21d270a5
redwane23 <redoianekassouse@gmail.com>
2024-08-26 00:12:55 +0100

    Update models.py

--- account/models.py
+++ account/models.py
@@ -3,11 +3,11 @@ from cloudinary.models import CloudinaryField
 from django.db import models
 
 class account(AbstractBaseUser, PermissionsMixin):
-  username = models.CharField(max_lenght=50,unique=True)
+  username = models.CharField(max_length=50,unique=True)
   email = models.EmailField(unique=True,null=True)
   is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
-  bio = models.CharField(max_lenght=200,blank=True)
+  bio = models.CharField(max_length=200,blank=True)
   profile_picture = CloudinaryField('image', blank=True, null=True)
   
   USERNAME_FIELD = 'username'
