01143267e0b97bce9fcc6d9bfd903968576848d2
redwane23 <redoianekassouse@gmail.com>
2024-08-25 23:12:48 +0100

    Update models.py

--- account/models.py
+++ account/models.py
@@ -2,10 +2,14 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
 class account(AbstractBaseUser, PermissionsMixin):
-  username = CharField(max_lenght=50)
+  username = CharField(max_lenght=50,unique=True)
   email = EmailField(unique=True,null=True)
   is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
-  bio = CharField(max_lenght=200)
-  password = CharField(max_lenght=25)
+  bio = CharField(max_lenght=200,blank=True)
   
+  USERNAME_FIELD = 'username'
+  REQUIRED_FIELDS = ['email']
+
+  def __str__(self):
+      return self.username
