d38e3e4fe547329acae597ecc63900c26a5fcc5f
redwane23 <redoianekassouse@gmail.com>
2024-08-25 17:48:14 +0100

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryImageField
+from cloudinary.models import CloudinaryField
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
-    profile_picture = CloudinaryImageField(required=False)
+    profile_picture = CloudinaryField('image', blank=True, null=True)
     class Meta:
         model = User
         fields = ['username','profile_picture']
