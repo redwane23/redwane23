d8a828a00b442f0e5bb895ec5d4fe0da1a215625
redwane23 <redoianekassouse@gmail.com>
2024-08-25 23:05:22 +0100

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,17 +1,25 @@
-from django import forms
-from django.contrib.auth.models import User
 from cloudinary.forms import CloudinaryFileField
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
-    profile_picture = CloudinaryFileField('image', required=False, label='Profile Picture')
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CreationForm(UserCreationForm):
+    password=forms.CharField(widget=forms.PasswordInput)
+    password2=forms.CharField(widget=forms.PasswordInput)
+    
     class Meta:
-        model = User
-        fields = ['username','profile_picture']
-
+        model = account
+        fields = ('username','email', 'bio', 'password')
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+
+
+        
