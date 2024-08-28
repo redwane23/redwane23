e736cc21003197d36d05cf1612a5ee122d02313a
redwane23 <redoianekassouse@gmail.com>
2024-08-28 10:46:38 +0100

    Update models.py

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-class account(AbstractBaseUser, PermissionsMixin):
+class account(AbstractUser):
   groups = models.ManyToManyField(Group, related_name="custom_user_set")
   
   user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions_set")
