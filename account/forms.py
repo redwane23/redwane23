482580d3d73962bbfab7c9c1803feb2a463fd325
redwane23 <redoianekassouse@gmail.com>
2024-08-29 20:43:56 +0100

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -4,17 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import account 
 
 class AccountCreationForm(UserCreationForm):
-    password=forms.CharField(label='password',widget=forms.PasswordInput)
-    password2=forms.CharField(label="confirm password",widget=forms.PasswordInput)
-    
     class Meta:
         model = account
         fields = ('username','email', 'bio','profile_picture')
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
-        return cd['password2']
 
 
 
